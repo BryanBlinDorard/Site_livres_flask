cb40119426b9265425e99ded411d03b5ef86aa50
Constantin Loszach <110570383+closzach@users.noreply.github.com>
2022-12-07 10:15:21 +0100

    Delete tuto_flask directory

--- tuto_flask/.flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=tuto
-FLASK_ENV=development
--- tuto_flask/tuto/__init__.py
+++
@@ -1,7 +0,0 @@
-from .app import app
-import tuto.views
-import tuto.commands
-import tuto.models
-
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy(app)
